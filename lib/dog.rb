5fd8e469c3d80597195185d9edc28ba2d7033a8d
Ryan K Burns <ryankburns@gmail.com>
2020-10-03 17:12:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,14 +3,28 @@ class Dog
 
 @@all = []
 
+attr_accessor :name
+
   def initialize(name)
     @name = name
-    @@all << self
   end
 
   def self.all
     @@all
   end
 
+  def self.clear_all
+    
+  end
 
+  def self.print_all
+    
+  end
+
+  def save
+    @@all << self
+  end
+
+  
+  
 end
